cc60d025c190eafd0a03fdf16914df6bdf6182c1
Nicole Oraya <nicole.oraya.cics@ust.edu.ph>
2023-04-04 23:44:35 +0800

    example navbar

--- components/navbar.php
+++ components/navbar.php
@@ -1,1 +1,37 @@
+<<<<<<< Updated upstream
 //example only add emerut
+=======
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="styles/global.css">
+    <link rel="stylesheet" href="styles/navbar.css">
+</head>
+<body>
+
+<nav>
+        <div class="logo">
+            TITLE
+        </div>
+        <input type="checkbox" id="click">
+        <label for="click" class="menu-btn">
+        
+        <i><ion-icon name="menu-outline"></ion-icon></i>
+        </label>
+        <ul>
+           <li><a class="active" href="#">PRODUCTS</a></li>
+           <li><a href="#">SEARCH</a></li>
+           <li><a href="#">ACCOUNT</a></li>
+        </ul>
+
+
+        <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
+>>>>>>> Stashed changes
