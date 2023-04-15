35df18ae1385794f179887ec1bce06cbe944df46
JuliaMelle <toledopascuajuliamelle@gmail.com>
2023-04-15 19:25:38 +0800

    LOGIN- not yet finish

---
+++ login.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- css -->
+  <link rel="stylesheet" href="styles/global.css">
+  <link rel="stylesheet" href="styles/login.css">
+</head>
+
+<body>
+
+  <?php require_once 'components/navbar.php' ?>
+  <div class="container">
+    <div class="card">
+
+      <div class="mid">
+        <h2 class="welcome">WELCOME</h2>
+        <!-- input username -->
+        <b>USERNAME</b>
+        <input type="text" class="input"></input>
+        <!-- input password -->
+        <b>PASSWORD</b>
+        <input type="password" class="input"></input>
+        
+        <button class="">LOGIN</button>
+        <button class="">LOGIN</button>
+      </div>
+
+ 
+    </div>
+
+  </div>
+  </div>
+  <?php require_once 'components/footer.php' ?>
+
+  <!-- <script src="" async defer></script> -->
+</body>
+
+</html>
