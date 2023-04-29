66dbe1421a0014998113993d319ea8659f358dfe
JuliaMelle <toledopascuajuliamelle@gmail.com>
2023-04-29 19:18:38 +0800

    yey nagana na yung backend ng registration form

--- backend/register_data.php
+++ backend/register_data.php
@@ -2,64 +2,116 @@
 
     //1. Setup Database connection
     require_once '../config.php';
-    $upload_dir= '../user_identification/';
+    $upload_dir = '../user_profile_img/';
+    $upload_dir2 = '../user_identification/';
     // $upload_dir = '../uploads/';
 
     $business_name = $_POST['business_name'];
     $username = $_POST['username'];
     $email = $_POST['email'];
     $address = $_POST['address'];
-    $password =$_POST['password'];
+    $password = $_POST['password'];
 
     $imgName = $_FILES['image']['name'];
-	$imgTmp = $_FILES['image']['tmp_name'];
-	$imgSize = $_FILES['image']['size'];
+    $imgTmp = $_FILES['image']['tmp_name'];
+    $imgSize = $_FILES['image']['size'];
 
     $imgExt = strtolower(pathinfo($imgName, PATHINFO_EXTENSION));
 
-			$allowExt  = array('jpeg', 'jpg', 'png', 'gif');
+    $allowExt  = array('jpeg', 'jpg', 'png', 'gif');
 
-			$userPic = time().'_'.rand(1000,9999).'.'.$imgExt;
+    $userPic = time() . '_' . rand(1000, 9999) . '.' . $imgExt;
 
-			if(in_array($imgExt, $allowExt)){
+    if (in_array($imgExt, $allowExt)) {
 
-	if($imgSize < 5000000){
-	move_uploaded_file($imgTmp,$upload_dir.$userPic);
-				}else{
-					$errorMsg = 'Image too large';
-				}
-			}else{
-				$errorMsg = 'Please select a valid image';
-			}
-		
+        if ($imgSize < 5000000) {
+            move_uploaded_file($imgTmp, $upload_dir . $userPic);
+        } else {
+            $errorMsg = 'Image too large';
+        }
+    } else {
+        $errorMsg = 'Please select a valid image';
+    }
 
+    // 2nd image
+    $imgName2 = $_FILES['image_v2']['name'];
+    $imgTmp2 = $_FILES['image_v2']['tmp_name'];
+    $imgSize2 = $_FILES['image_v2']['size'];
+
+    $imgExt2 = strtolower(pathinfo($imgName2, PATHINFO_EXTENSION));
+
+    $allowExt  = array('jpeg', 'jpg', 'png', 'gif');
+
+    $userPic2 = time() . '_' . rand(1000, 9999) . '.' . $imgExt2;
+
+    if (in_array($imgExt, $allowExt)) {
+
+        if ($imgSize2 < 5000000) {
+            move_uploaded_file($imgTmp2, $upload_dir2 . $userPic2);
+        } else {
+            $errorMsg = 'Image too large';
+        }
+    } else {
+        $errorMsg = 'Please select a valid image';
+    }
+
+    if(empty($business_name)){
+        $errorMsg ='Please input your Display name';
+        header('Location: ../registration_form.php?authenticate=false');
+      } else if(empty($username)){
+        $errorMsg ='Please input your username';
+        header('Location: ../registration_form.php?authenticate=false');
+      }
+     else if(empty($email)){
+        $errorMsg ='Please input your email';
+        header('Location: ../registration_form.php?authenticate=false');
+      }
+      else if(empty($password)){
+        $errorMsg ='Please input your password';
+        header('Location: ../registration_form.php?authenticate=false');
+      }
+      else if(empty($address)){
+        $errorMsg ='Please input your address';
+        header('Location: ../registration_form.php?authenticate=false');
+      }
+      else if(empty($_FILES['image']["tmp_name"])){
+        $errorMsg ='Please input valid files';
+        header('Location: ../registration_form.php?authenticate=false');
+      }
+      else if(empty($_FILES['imgName2']["imgTmp2"])){
+        $errorMsg ='Please input valid files';
+        header('Location: ../registration_form.php?authenticate=false');
+      }
+      
     //2. Insert SQL
+    else {
     $sql = "  INSERT INTO 
     `users`
     (`business_name`, `username`, `email`, `password`, `address`, `profile_img`, `valid_id_img`) 
     VALUES (
-        '".$business_name."',
-        '".$username."',
-        '".$email."',
-        '".$password."',
-        '".$address."',
-        '".$userPic."',
-        'heh'
+        '" . $business_name . "',
+        '" . $username . "',
+        '" . $email . "',
+        '" . $password . "',
+        '" . $address . "',
+        '" . $userPic . "',
+        '" . $userPic2 . "'
         )";
 
 
     //3. Execute SQL
     if (mysqli_query($conn, $sql)) {
-    header('Location:../login.php');
+        header('Location:../login.php');
     } else {
-    mysqli_error($conn);
-    header('Location: ../registration_form?authenticate=false');
+        mysqli_error($conn);
+        header('Location: ../registration_form?authenticate=false');
     }
 
     //.4 Closing Database Connection
     mysqli_close($conn);
- 
-    
+    }
+
+
 
 
     ?>
--- registration_form.php
+++ registration_form.php
@@ -4,90 +4,93 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  
+
   <title>LuzViMinda | Register</title>
-  
+
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  
+
   <!-- css files -->
   <link rel="stylesheet" href="styles/global.css">
   <link rel="stylesheet" href="styles/registration_form.css">
 </head>
 
 <body>
-<script type="text/javascript" src="validate.js"></script>
+  <script type="text/javascript" src="validate.js"></script>
 
   <?php require_once 'components/navbar.php' ?>
   <div class="container">
     <div class="mid_container">
-    <form class="form-flex column" id="regForm" action="backend/register_data.php" method="post" enctype="multipart/form-data">
 
-      <div class="card"> <!-- CARD -->
-        <h2 class="registerheader">JOIN US</h2>
+      <form class="form-flex column" id="regForm" action="backend/register_data.php" method="post" enctype="multipart/form-data">
 
-        <h3 class="register">LOGIN CREDENTIALS</h3>
-        <!-- input username -->
-        <b>USERNAME</b>
-        <input type="text" class="input" name="username"></input>
-        
-        <!-- input password -->
-        <b>PASSWORD</b>
-        <input type="password" class="input" name="password"></input>
-        <br>
-        <br>
+        <div class="card"> <!-- CARD -->
+          <h2 class="registerheader">JOIN US</h2>
 
-        <h3 class="register">BUSINESS INFORMATION</h3>
-        <!-- input display name -->
-        <b>DISPLAY NAME</b>
-        <input type="displayname" class="input" name="business_name" ></input>
-        
-        <!-- input email -->
-        <b>EMAIL</b>
-        <input type="email" class="input" name="email"></input>
+          <h3 class="register">LOGIN CREDENTIALS</h3>
+          <!-- input username -->
+          <b>USERNAME</b>
+          <input type="text" class="input" name="username" required></input>
 
-        <!-- input address -->
-        <b>ADDRESS</b>
-        <input type="address" class="input" name="address"></input>
+          <!-- input password -->
+          <b>PASSWORD</b>
+          <input type="password" class="input" name="password" required></input>
+          <br>
+          <br>
 
-         <!-- input profile picture -->
-         <b>PROFILE PICTURE</b>
-        <input type="file" class="upload-btn" name="image"></input>
-        <br>
+          <h3 class="register">BUSINESS INFORMATION</h3>
+          <!-- input display name -->
+          <b>DISPLAY NAME</b>
+          <input type="displayname" class="input" name="business_name" required></input>
 
-        <!-- input profile valid ID -->
-        <b>VALID ID</b>
-        <input type="file" class="upload-btn" name="image"></input>
-        <br>
-    
-        <div class="mid_position_buttons">
-        <button type="submit" name="Submit" class="button login" onclick="validateForm()">SUBMIT</button>
-        </div>
-        </form>
-      </div>
-   
+          <!-- input email -->
+          <b>EMAIL</b>
+          <input type="email" class="input" name="email" required> </input>
+
+          <!-- input address -->
+          <b>ADDRESS</b>
+          <input type="address" class="input" name="address" required></input>
+
+          <!-- input profile picture -->
+          <b>PROFILE PICTURE</b>
+          <input type="file" class="upload-btn" name="image" required></input>
+          <br>
+
+          <!-- input profile valid ID -->
+          <b>VALID ID</b>
+          <input type="file" class="upload-btn" name="image_v2" required></input>
+          <br>
+
+          <div class="mid_position_buttons">
+            <button type="submit" name="Submit" class="button login" onclick="validateForm()">SUBMIT</button>
+          </div>
+      </form>
+      <?php
+      if (isset($_GET['authenticate'])) { //check if authenticate key exists in URL
+        if ($_GET['authenticate'] == "false") {
+      ?>
+          <br>
+          <div class="alert">
+            <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+            <strong style="color:white">Please fill out all required fields.</strong>
+          </div>
+
+      <?php
+        }
+      }
+      ?>
     </div>
+
+  </div>
   </div>
   <br>
   <br>
   <br>
 
-  <?php
-    if (isset($_GET['authenticate'])) { //check if authenticate key exists in URL
-      if ($_GET['authenticate'] == "false") {
-        echo '
-          <br>
-          <div class="alert alert-danger" role="alert">
-            Invalid inputs.
-          </div>
-        ';
 
-      }
-    }
-    ?>
 
   <?php require_once 'components/footer.php' ?>
 
 </body>
- 
+
 </html>
