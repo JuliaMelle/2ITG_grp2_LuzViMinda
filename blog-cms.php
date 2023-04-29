cf1cbbbeda3f58e3eda1f4fa6880073ec57f4377
Nicole Oraya <nicole.oraya.cics@ust.edu.ph>
2023-04-29 16:07:11 +0800

    update blog cms
    database support: can now do create, update, and delete

--- blog-cms.php
+++ blog-cms.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,99 +19,48 @@
     <?php require_once 'components/navbar.php' ?>
 
     <div class="blog-container">
-        <button class="button" id="btn-add">ADD A POST</button>
-        <?php
-        
-        require_once 'config.php';
-        $sql = "SELECT * FROM blog_post";
-        if ($result = $mysqli->query($sql)) {
-            if ($result->num_rows > 0) {
-        echo '<table class="a">';
-        echo    '<thead>';
-        echo        '<tr>';
-        echo            '<th class="id">ID</th>';
-        echo           ' <th class="title">BLOG TITLE</th>';
-        echo           ' <th class="title">BLOG CONTENT</th>';
-        echo           '<th class="delete">DELETE</th>';
-        echo           '<th class="edit">EDIT</th>';
-        echo        '</tr>';
-        echo    '</thead>';
-        echo    '<tbody>';
-        while ($row = $result->fetch_array()) {
-        echo        '<tr>';
-        echo            '<td class="blog"><span class="blog-id">' . $row['post_id'] . '</span></td>';
-        echo            '<td><span class="blog-title">' . $row['title'] . '</span></td>';
-        echo            '<td><span class="blog-title">' . $row['content'] . '</span></td>';
-        echo            '<td><span class="blog-delete"><i class="fa-regular fa-trash-can"></i></span></td>';
-        echo            '<td><span class="blog-edit"><i class="fa-regular fa-pen-to-square"></i></span></td>';
-        echo        '</tr>';
-        }
-        echo    '</tbody>';
-        echo '</table>';
-        $result->free();
-    } else {
-        echo '<div class="alert alert-danger"><em>No records were found.</em></div>';
-    }
-} else {
-    echo "Oops! Something went wrong. Please try again later.";
-}
+        <a href="add-blog.php"><button class="button">ADD A POST</button></a>
+        <!-- id="btn-add" -->
+        <table class="a">
+            <thead>
+                <tr>
+                    <th class="id">POST ID</th>
+                    <th class="title">BLOG TITLE</th>
+                    <th class="title">BLOG CONTENT</th>
+                    <th class="delete">DELETE</th>
+                    <th class="edit">EDIT</th>
+                </tr>
+            </thead>
+            <tbody>
 
-// Close connection
-$mysqli->close();
-?>
+                <?php
+                require_once 'config.php';
+                $sql = "SELECT * FROM blog_post";
+                if ($result = $mysqli->query($sql)) {
+                    if ($result->num_rows > 0) {
 
-    </div>
+                        while ($row = $result->fetch_array()) {
+                            echo '<tr>';
+                            echo '<td class="blog"><span class="blog-id">' . $row['post_id'] . '</span></td>';
+                            echo '<td><span class="blog-title">' . $row['title'] . '</span></td>';
+                            echo '<td><span class="blog-title">' . $row['content'] . '</span></td>';
+                            // echo '<td><a id="btn-add" href="backend/blog-delete2.php?post_id=' . $row['post_id'] . '" title="Delete Record" data-toggle="tooltip"><span class="blog-delete"><i class="fa-regular fa-trash-can"></i></span></a></td>';
 
-    <!-- ADD POST MODAL -->
-    <div id="add-modal" class="modal">
+                            echo '<td><a href="backend/blog-delete.php?post_id=' . $row['post_id'] . '"title="Delete Record" data-toggle="tooltip"><span class="blog-delete" onclick="return confirm('."'". "Are you sure?" ."'".');"><i class="fa-regular fa-trash-can"></i></span></a></td>';
+                            echo '<td><a href="edit-blog.php?post_id=' . $row['post_id'] . '" title="Edit Record" data-toggle="tooltip"><span class="blog-edit"><i class="fa-regular fa-pen-to-square"></i></span></a></td>';
+                            
+                        }
+                    }
+                }
+                ?>
 
-        <!-- Modal content -->
-        <div class="modal-content">
-            <span class="close">&times;</span>
-            <hr>
+                </tr>
+            </tbody>
+        </table>
 
-            <form action="">
-                <p>TITLE</p>
-                <input type="text" name="" id="" placeholder='Title here'>
-                <span class="blog-category">BLOG</span>
-                <p>BLOG CONTENT</p>
-                <textarea placeholder='Enter comment...' maxlength='10000' minlength='100'></textarea>
-                <button type="submit" class="button" id="myBtn">PUBLISH POST</button>
-            </form>
-
-        </div>
 
     </div>
-
     <?php require_once 'components/footer.php' ?>
-
-    <script>
-        // Get the modal
-        var modal = document.getElementById("add-modal");
-
-        // Get the button that opens the modal
-        var btn = document.getElementById("btn-add");
-
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
-
-        // When the user clicks on the button, open the modal
-        btn.onclick = function() {
-            modal.style.display = "block";
-        }
-
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() {
-            modal.style.display = "none";
-        }
-
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-        }
-    </script>
 </body>
 
 </html>
---
+++ edit-blog.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta title="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Post</title>
+    <link rel="stylesheet" href="styles/global.css">
+    <link rel="stylesheet" href="styles/add-post.css">
+
+    <script src="https://kit.fontawesome.com/b1d36f5527.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <?php
+    require_once "config.php";
+    if (isset($_GET["post_id"])) {
+        $sql = "SELECT * FROM blog_post WHERE post_id = ?";
+
+        if ($stmt = $mysqli->prepare($sql)) {
+            // Bind variables to the prepared statement as parameters
+            $stmt->bind_param("i", $param_id);
+
+            // Set parameters
+            $param_id = trim($_GET["post_id"]);
+
+            // Attempt to execute the prepared statement
+            if ($stmt->execute()) {
+                $result = $stmt->get_result();
+
+                if ($result->num_rows == 1) {
+                    /* Fetch result row as an associative array. Since the result set
+                            contains only one row, we don't need to use while loop */
+                    $row = $result->fetch_array(MYSQLI_ASSOC);
+
+                    // FORM
+                    $title = $row['title'];
+                    $content = $row["content"];
+                } else {
+                    // URL doesn't contain valid id parameter. Redirect to error page
+                    header("location: error.php");
+                    exit();
+                }
+            } else {
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+        // Close statement
+        $stmt->close();
+
+        // Close connection
+        $mysqli->close();
+    } else {
+        // URL doesn't contain id parameter. Redirect to error page
+        header("location: error.php");
+        exit();
+    }
+    ?>
+
+    <div class="wrapper">
+        <div class="wrapper-content">
+            <p>ADD A BLOG</p>
+            <hr>
+            <form action="backend/blog-edit.php" method="post">
+                <p>TITLE</p>
+                <input required type="text" name="title" id="blog_title" placeholder="<?php echo $row["title"]; ?>">
+                <span class="blog-category">BLOG</span>
+                <p>BLOG CONTENT</p>
+                <textarea required name="content" maxlength='10000' minlength='100'><?php echo $row["content"]; ?></textarea>
+                <input type="hidden" name="id" value="<?php echo $_GET["post_id"]; ?>" />
+                <button type="submit" class="button" title="submit" id="submit">PUBLISH POST</button>
+            </form>
+        </div>
+    </div>
+
+
+    <?php require_once 'components/footer.php' ?>
+</body>
+
+</html>
