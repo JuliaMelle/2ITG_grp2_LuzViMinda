38c05431b16f2cccb3877b720317a4dfa5d49fbb
JuliaMelle <toledopascuajuliamelle@gmail.com>
2023-04-29 16:48:02 +0800

    Barely nagana HAHA

--- registration_form.php
+++ registration_form.php
@@ -16,10 +16,12 @@
 </head>
 
 <body>
+<script type="text/javascript" src="validate.js"></script>
 
   <?php require_once 'components/navbar.php' ?>
   <div class="container">
     <div class="mid_container">
+    <form class="form-flex column" id="regForm" action="backend/register_data.php" method="post" enctype="multipart/form-data">
 
       <div class="card"> <!-- CARD -->
         <h2 class="registerheader">JOIN US</h2>
@@ -27,44 +29,63 @@
         <h3 class="register">LOGIN CREDENTIALS</h3>
         <!-- input username -->
         <b>USERNAME</b>
-        <input type="text" class="input"></input>
+        <input type="text" class="input" name="username"></input>
         
         <!-- input password -->
         <b>PASSWORD</b>
-        <input type="password" class="input"></input>
+        <input type="password" class="input" name="password"></input>
 
         <h3 class="register">BUSINESS INFORMATION</h3>
         <!-- input display name -->
         <b>DISPLAY NAME</b>
-        <input type="displayname" class="input"></input>
+        <input type="displayname" class="input" name="business_name" ></input>
         
         <!-- input email -->
         <b>EMAIL</b>
-        <input type="email" class="input"></input>
+        <input type="email" class="input" name="email"></input>
 
         <!-- input address -->
         <b>ADDRESS</b>
-        <input type="address" class="input"></input>
+        <input type="address" class="input" name="address"></input>
 
         <!-- input profile picture -->
         <b>PROFILE PICTURE</b>
-        <input type="file" id="picture"></input>
+        <input type="file"  name="image"></input>
         <label for="picture" class="profpic">UPLOAD YOUR PROFILE PICTURE</label>
+        <br>
+        <!-- input profile valid ID -->
+        <!-- <b>VALID ID</b>
+        <input type="file" id="picture" name="imagevalid" style="color:black;"  ></input>
+        <label for="picture" class="profpic" >UPLOAD YOUR VALID ID</label> -->
         
+  
+    
         <div class="mid_position_buttons">
-        <button class="button login">SUBMIT</button>
+        <button type="submit" name="Submit" class="button login" onclick="validateForm()">SUBMIT</button>
         </div>
-
+        </form>
       </div>
-
+   
     </div>
-
   </div>
   <br>
   <br>
   <br>
+  <?php
+    if (isset($_GET['authenticate'])) { //check if authenticate key exists in URL
+      if ($_GET['authenticate'] == "false") {
+        echo '
+          <br>
+          <div class="alert alert-danger" role="alert">
+            Invalid inputs.
+          </div>
+        ';
 
+      }
+    }
+    ?>
   <?php require_once 'components/footer.php' ?>
 
 </body>
+ 
 </html>
