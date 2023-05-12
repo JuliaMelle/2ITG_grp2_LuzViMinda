76b377ff7853a6a29d9a44cf05303892d678947b
JuliaMelle <toledopascuajuliamelle@gmail.com>
2023-05-12 22:21:48 +0800

    search emerut

--- general/product-catalog.php
+++ general/product-catalog.php
@@ -26,50 +26,117 @@ session_start();
 </head>
 
 <body>
-    <?php 
+    <?php
     if (isset($_SESSION['loggedin'])) {
         require_once '../components/navbar-seller.php';
-    }
-        else{
+    } else {
         require_once '../components/navbar-general.php';
-    
     }
     ?>
     <div class="container">
-        <div class="mid_container">
 
-        <?php
-        $sql = "SELECT* FROM products";
-        $result = mysqli_query($conn, $sql);
-                    $resultCheck = mysqli_num_rows($result);
-        
-                  if ($resultCheck > 0) {
-                        while ($row = mysqli_fetch_assoc($result)) {  
-        ?>
-
-        <a href="view_product_specific?id=<?php echo $row['product_id'] ?>" class="product-text sub-link">
-
-            <div class="card"> <!-- CARD -->
-                <div class="capsule">
-                    <p class="capsule_caption">   <?php echo $row ['category'] ?> </p>
-                </div>
-                <img src="../img/temp.png" alt="Avatar" style="width:100%">
-                <h4 class="head">
-                <?php echo $row ['product_name'] ?> 
-                </h4>
-                <h5 class="read_more">
-                <?php echo $row ['seller_name'] ?>
-                </h5>
-            </div>
-
-
-            <?php }
-           }?>
-
-            <!-- MID CONTAINER END -->
+        <div class="input-container">
+            <form action="product-catalog.php" method="get">
+                <input type="text" name="text" class="input" placeholder="search..." aria-label="Search" name="query">
+            </form>
+            <span class="icon">
+                <svg width="19px" height="19px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                    <g id="SVGRepo_iconCarrier">
+                        <path opacity="1" d="M14 5H20" stroke="#000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                        <path opacity="1" d="M14 8H17" stroke="#000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                        <path d="M21 11.5C21 16.75 16.75 21 11.5 21C6.25 21 2 16.75 2 11.5C2 6.25 6.25 2 11.5 2" stroke="#000" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                        <path opacity="1" d="M22 22L20 20" stroke="#000" stroke-width="3.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                    </g>
+                </svg>
+            </span>
         </div>
-    </div>
-    <?php require_once '../components/footer.php' ?>
+        <?php
+
+
+        $sSearch = "";
+        if (isset($_REQUEST['query'])) {
+            $sSearch = $_REQUEST['query'];
+        }
+        $sql = "SELECT* FROM products";
+        
+       
+        if ($sSearch <> "") { ?>
+            <!-- SEARCH PAGE -->
+            <?php
+            $sql = "SELECT* FROM products p JOIN users us ON us.user_id=p.user_id WHERE `product_name` LIKE  '%" . $sSearch . "%' or `category` LIKE  '%" . $sSearch . "%'or `seller_name` LIKE  '%" . $sSearch . "%'";
+            ?>
+            <!-- CARD -->
+            <div class="mid_container">
+                <h1 class="center"><?php echo " " . $sSearch; ?></h1>
+
+                <?php
+                $upload_dir = '../upload/';
+                $result = mysqli_query($conn, $sql);
+                $resultCheck = mysqli_num_rows($result);
+
+                if ($resultCheck > 0) {
+                    while ($row = mysqli_fetch_assoc($result)) {
+                ?>
+
+                        <a href="view_product_specific?id=<?php echo $row['product_id'] ?>" class="product-text sub-link">
+
+                            <div class="card"> <!-- CARD -->
+                                <div class="capsule">
+                                    <p class="capsule_caption"> <?php echo $row['category'] ?> </p>
+                                </div>
+                                <img src="../img/temp.png" alt="Avatar" style="width:100%">
+                                <h4 class="head">
+                                    <?php echo $row['product_name'] ?>
+                                </h4>
+                                <h5 class="read_more">
+                                    <?php echo $row['seller_name'] ?>
+                                </h5>
+                            </div>
+                            <!-- CARD -->
+                    <?php
+                    }
+                }
+            } else { ?>
+
+                    <div class="mid_container">
+
+                        <?php
+                       
+                        $result = mysqli_query($conn, $sql);
+                        $resultCheck = mysqli_num_rows($result);
+
+                        if ($resultCheck > 0) {
+                            while ($row = mysqli_fetch_assoc($result)) {
+                        ?>
+
+                                <a href="view_product_specific?id=<?php echo $row['product_id'] ?>" class="product-text sub-link">
+
+                                    <div class="card"> <!-- CARD -->
+                                        <div class="capsule">
+                                            <p class="capsule_caption"> <?php echo $row['category'] ?> </p>
+                                        </div>
+                                        <img src="../img/temp.png" alt="Avatar" style="width:100%">
+                                        <h4 class="head">
+                                            <?php echo $row['product_name'] ?>
+                                        </h4>
+                                        <h5 class="read_more">
+                                            <?php echo $row['seller_name'] ?>
+                                        </h5>
+                                    </div>
+
+
+                            <?php }
+                        } 
+            }
+                 ?>
+
+                 
+                        <!-- MID CONTAINER END -->
+                    </div>
+            </div>
+            <?php require_once '../components/footer.php' ?>
 
 </body>
 
